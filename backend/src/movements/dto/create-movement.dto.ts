5e343cb76574b6bd38e8603edf41089e0f0e96fd
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2026-02-25 20:33:04 -0300

    backend-movement(create-dto): fix month value in function

--- backend/src/movements/dto/create-movement.dto.ts
+++ backend/src/movements/dto/create-movement.dto.ts
@@ -21,7 +21,7 @@ import {
 
 const formatDateToDDMMYYYY = (date: Date): string => {
   const day = date.getDate().toString().padStart(2, '0');
-  const month = date.getMonth().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
 
   return `${day}-${month}-${year}`;
